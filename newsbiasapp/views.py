f9e89f5ab9f399775b9819458609fd432cda65a4
jlin816 <lnj@mit.edu>
2016-02-14 02:10:05 -0800

    what's happening

--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 import requests
 import os
+from textblob import TextBlob
 
 def getData(request):
     #f = open("./api_key.txt", "r")
     #key = f.read().strip()
     query = request.GET['q']
+    print query
     query_parts = query.split()
     ALCHEMY_SECRET_KEY = os.environ['ALCHEMY_SECRET_KEY']
     url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + ALCHEMY_SECRET_KEY +"&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json"
