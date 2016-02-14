995fcd10cd5f40931953d697bb2c0aab3928fad8
jlin816 <lnj@mit.edu>
2016-02-13 21:31:27 -0800

    Unsafe hack for API Key

--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -2,12 +2,11 @@ from django.http import HttpResponse
 import requests
 
 def getData(request):
-    f = open("./api_key.txt", "r")
-    key = f.read().strip()
+    #f = open("./api_key.txt", "r")
+    #key = f.read().strip()
     # query = request.GET[''] #edit this
     query = "bernie sanders"
-    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + key \
-        + "&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
+    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=922bb143cb941e2501500df6406f4c301a02d03c&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
     response = requests.get(url)
     s = response.text
     
