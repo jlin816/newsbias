652bf07c29f6fa279d6d32241e6b2c47bbc7b059
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2016-02-13 21:49:03 -0800

    Secures API Key

--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -5,8 +5,9 @@ def getData(request):
     #f = open("./api_key.txt", "r")
     #key = f.read().strip()
     # query = request.GET[''] #edit this
+    ALCHEMY_SECRET_KEY = os.environ['ALCHEMY_SECRET_KEY']
     query = "bernie sanders"
-    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=922bb143cb941e2501500df6406f4c301a02d03c&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
+    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + ALCHEMY_SECRET_KEY +"&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
     response = requests.get(url)
     s = response.text
     
