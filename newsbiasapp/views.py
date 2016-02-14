2d27eb6bc2c28052e97c70d639de9759a13ef0d6
jlin816 <lnj@mit.edu>
2016-02-13 21:47:30 -0800

    Testing multiword queries

--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -5,8 +5,8 @@ def getData(request):
     #f = open("./api_key.txt", "r")
     #key = f.read().strip()
     # query = request.GET[''] #edit this
-    query = "trump sanders"
-    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=922bb143cb941e2501500df6406f4c301a02d03c&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
+    query = "sanders"
+    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=f79fa10c4829a73ad67cd90f2670b732644d999b&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
     response = requests.get(url)
     s = response.text
     
