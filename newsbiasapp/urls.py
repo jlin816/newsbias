78a71be6250b6b4852ef072fe0987e7302740613
jlin816 <lnj@mit.edu>
2016-02-13 20:41:49 -0800

    Integrated AlchemyAPI

--- newsbiasapp/urls.py
+++ newsbiasapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [ 
-    url(r'^$',views.index,name='index'),
+    url(r'^getData',views.getData,name='getData'),
 ]
--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+import requests
 
-def index(request):
-    return HttpResponse("At index")
+def getData(request):
+    f = open("newsbiasapp/api_key.txt", "r")
+    key = f.read().strip()
+    # query = request.GET[''] #edit this
+    query = "bernie sanders"
+    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + key \
+        + "&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
+    response = requests.get(url)
+    s = response.text
+    
+    return HttpResponse(s)
     
