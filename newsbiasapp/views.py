7ab9d253fc9dcabd24bcc9f38a37bd263bdaaa18
jlin816 <lnj@mit.edu>
2016-02-13 21:59:28 -0800

    Handles multiword queries now.

--- newsbiasapp/views.py
+++ newsbiasapp/views.py
@@ -6,7 +6,11 @@ def getData(request):
     #key = f.read().strip()
     # query = request.GET[''] #edit this
     query = "trump sanders"
-    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=f79fa10c4829a73ad67cd90f2670b732644d999b&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json&q.enriched.url.enrichedTitle.keywords.keyword.text=" + query
+    query_parts = query.split()
+    url= "https://access.alchemyapi.com/calls/data/GetNews?apikey=f79fa10c4829a73ad67cd90f2670b732644d999b&return=enriched.url.title,enriched.url.url,enriched.url.docSentiment&start=1454803200&end=1455490800&count=25&outputMode=json"
+
+    for part in query_parts:
+        url += "&q.enriched.url.enrichedTitle.keywords.keyword.text=" + part
     response = requests.get(url)
     s = response.text
     
